66808364787ac039c9f56fca49fb6abbab687841
Samanth18 <ppipperasamanth@gmail.com>
2025-02-27 10:23:49 +0530

    Update LaunchAnEmptyBrowser.java

--- src/Sample/LaunchAnEmptyBrowser.java
+++ src/Sample/LaunchAnEmptyBrowser.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class LaunchAnEmptyBrowser {
-
+//Changes done in github
 	public static void main(String[] args) {
 //     ChromeDriver driver=new ChromeDriver();
      WebDriver driver=new EdgeDriver();
